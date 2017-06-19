552585d79b56a9ced85e82c8df726fe50418ffbc
Marsel <marsel.perdoci@hotmail.com>
2017-06-19 14:21:40 +0200

    Small changes to test app without auth on heroku

--- src/main/java/uni/kassel/web17/spring/Main.java
+++ src/main/java/uni/kassel/web17/spring/Main.java
@@ -20,7 +20,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EnableSwagger2 scans through all application classes and generates
 //documentation based on request param/returns/annotations etc 
-@EnableSwagger2
+
+//@EnableSwagger2
 @SpringBootApplication
 public class Main {
 	
@@ -30,14 +31,15 @@ public class Main {
 		
 	}
 	
-	@Bean
-	public Docket api(){
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-			
-	}
+	
+//	@Bean
+//	public Docket api(){
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//			
+//	}
 
 }
