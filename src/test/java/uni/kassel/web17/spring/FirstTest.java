4d896b61e83191f4d29218a1fc3766251ec5a911
Marsel <marsel.perdoci@hotmail.com>
2017-08-24 01:29:01 +0200

    Test get request on a certain post
    send a get request on the post endpoint /api/post/1 to retrieve this post
    which belongs to the user michael and assert if the respond contains the
    char sequence "michael". If so, test is successful.
    
    Note! Main Application must be running when running the test as its not
    being initialised upon test run.

---
+++ src/test/java/uni/kassel/web17/spring/FirstTest.java
@@ -0,0 +1,39 @@
+package uni.kassel.web17.spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+import springfox.documentation.spring.web.json.Json;
+import uni.kassel.web17.spring.post.Post;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FirstTest  {
+
+    @Test
+    public void testIt() throws IOException {
+
+        URL url = new URL("http://localhost:8080/api/post/1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        int status = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        String s = content.toString();
+
+        Assert.assertTrue(s.contains("comment-5"));
+
+        in.close();
+        con.disconnect();
+    }
+
+}
